0f25ea034ed15071c0bdf97f12b6c39733120c71
hrishikeshprateek <prateekhrishikesh@gmail.com>
2022-02-24 17:38:49 +0530

    partB programs

---
+++ src/partA/Threads.java
@@ -0,0 +1,68 @@
+package partA;
+
+public class Threads {
+    public static void main(String... args) {
+        Printer print = new Printer();
+        new Thread(new TaskEvenOdd(print, 10, false)).start();
+        new Thread(new TaskEvenOdd(print, 10, true)).start();
+    }
+}
+
+class TaskEvenOdd implements Runnable {
+
+    private int max;
+    private Printer print;
+    private boolean isEvenNumber;
+
+    TaskEvenOdd(Printer print, int max, boolean isEvenNumber) {
+        this.print = print;
+        this.max = max;
+        this.isEvenNumber = isEvenNumber;
+    }
+
+    @Override
+    public void run() {
+        int number = isEvenNumber == true ? 2 : 1;
+        while (number <= max) {
+            if (isEvenNumber) {
+                print.printEven(number);
+            } else {
+                print.printOdd(number);
+            }
+            number += 2;
+        }
+
+    }
+
+}
+
+class Printer {
+    boolean isOdd = false;
+    synchronized void printEven(int number) {
+
+        while (!isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Even:" + number);
+        isOdd = false;
+        notifyAll();
+    }
+
+    synchronized void printOdd(int number) {
+        while (isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Odd:" + number);
+        isOdd = true;
+        notifyAll();
+    }
+
+}
